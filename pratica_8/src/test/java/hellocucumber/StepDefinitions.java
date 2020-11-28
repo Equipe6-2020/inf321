6058cbe56e61df1cf452593e414e50e8f94484a2
Agner Esteves <agner_esteves@hotmail.com>
2020-11-28 11:38:45 -0300

    Refactory production class

--- pratica_8/src/test/java/hellocucumber/StepDefinitions.java
+++ pratica_8/src/test/java/hellocucumber/StepDefinitions.java
@@ -6,12 +6,6 @@ import io.cucumber.java.en.When;
 
 import static org.junit.Assert.*;
 
-class IsItFriday {
-    static String isItFriday(String today) {
-        return "Friday".equals(today) ? "TGIF" : "Nope";
-    }
-}
-
 
 public class StepDefinitions {
 
