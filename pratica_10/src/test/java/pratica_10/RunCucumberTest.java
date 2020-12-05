5ec79689398e73134d258838f28b4017f59fe8e2
esoft25 - Jose Eduardo Porte <ex145246@g.unicamp.br>
2020-12-05 10:58:19 -0300

    add pratica_10

---
+++ pratica_10/src/test/java/pratica_10/RunCucumberTest.java
@@ -0,0 +1,11 @@
+package pratica_10;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCucumberTest {
+
+}
---
+++ pratica_10/src/test/java/pratica_10/StepDefinitions.java
@@ -0,0 +1,11 @@
+package pratica_10;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class StepDefinitions {
+
+}
